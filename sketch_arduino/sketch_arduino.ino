1863e2260ab4a97e561247e9e2fa0ac5b8ba208c
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-23 19:46:58 -0300

    MOD - sketch

--- sketch_arduino/sketch_arduino.ino
+++ sketch_arduino/sketch_arduino.ino
@@ -1,9 +1,11 @@
+
 bool led_builtin_on = false;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
@@ -12,10 +14,5 @@ void loop() {
     String data = Serial.readStringUntil('\n');
     Serial.print("You sent me: ");
     Serial.println(data);
-  }
-  else {
-    led_builtin_on = (led_builtin_on) ? false : true;
-    digitalWrite(LED_BUILTIN, led_builtin_on);
-    delay(500);
   }
 }
