50d684f4a37eb79479d820aa056e86b0c32ef74c
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-23 21:01:15 -0300

    ADD - getDistance()

--- sketch_arduino/sketch_arduino.ino
+++ sketch_arduino/sketch_arduino.ino
@@ -1,5 +1,9 @@
 #include<EEPROM.h>
 
+#define TRIG 8
+#define ECHO 9
+#define VALVE_PIN A5
+
 typedef struct data  // data to store in EEPROM
 {
   float capacity;
@@ -10,6 +14,9 @@ Data data;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(VALVE_PIN, OUTPUT);
   EEPROM.get(0, data);  // recovers the previous state
 }
 
@@ -20,4 +27,20 @@ void loop() {
     Serial.print("You sent me: ");
     Serial.println(data);
   }
+}
+
+
+float getDistance() {
+  float echoTime;
+  float distance = -1;
+  
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  echoTime = pulseIn(ECHO, HIGH);
+
+  distance = echoTime / 58;
+
+  return distance;
 }
