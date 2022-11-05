fe21abd34cac6f52e449a03fa0cef960b2b13c4f
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-05 17:44:02 -0300

    ADD - Testing serial coms

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import serial
 import time
+import cv2 as cv
+
 if __name__ == '__main__':
 
     connected = False
@@ -9,7 +11,7 @@ if __name__ == '__main__':
     for i in range(0, 20):
         try:
             port = 'com' + str(i)
-            ser = serial.Serial(port, 9600, timeout=1)  # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            ser = serial.Serial(port, 9600, timeout=5)  # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
             ser.reset_input_buffer()
         except:
             continue
@@ -21,8 +23,11 @@ if __name__ == '__main__':
     if not connected:
         print("Could not connect serial")
 
-    while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+    while connected:
+        ser.write(b"get levl\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
+        if cv.waitKey(1) & 0xff == ord('q'):
+            ser.close()
+            print("Connection closed")
