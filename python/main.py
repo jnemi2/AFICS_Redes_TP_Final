1f34df23a47ce28c9c72ce5e530b96bff7961ce8
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-05 17:30:06 -0300

    ADD - serial handshake

--- python/main.py
+++ python/main.py
@@ -2,8 +2,25 @@
 import serial
 import time
 if __name__ == '__main__':
-    ser = serial.Serial('com3', 9600, timeout=1)  # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
+
+    connected = False
+    ser = None
+    port = 'com3'
+    for i in range(0, 20):
+        try:
+            port = 'com' + str(i)
+            ser = serial.Serial(port, 9600, timeout=1)  # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            ser.reset_input_buffer()
+        except:
+            continue
+        else:
+            print("Connected to port", port)
+            connected = True
+            break
+
+    if not connected:
+        print("Could not connect serial")
+
     while True:
         ser.write(b"Hello from Raspberry Pi!\n")
         line = ser.readline().decode('utf-8').rstrip()
